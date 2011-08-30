45c51a05961f536bac86e01fd66b8884c52f6bc7
unknown <User@.(none)>
2011-08-30 09:37:13 +0200

    Add some functions to the cart.

--- src/core/Cart.java
+++ src/core/Cart.java
@@ -2,7 +2,17 @@ package core;
 
 /**
  * (c) utcl95 - 21/08/11
+ * add      - Add products to the cart.
+ * delete   -
+ * show     - show all products in the cart.
+ * get      - get product by id/name
+ * number   - get numbers of products in the cart.
  */
 public class Cart {
+    public Cart() {
+    }
 
+    public void add() {}
+
+    public void delete(int Id) {}
 }
